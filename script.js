a51869270365294414799755be0ad6e32be99f67
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-11-25 20:37:34 +0200

    chore commenting whole script

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-
+// Available choices for the game
 const choices = ["rock", "paper", "scissors"];
+
+// Scores for the player and computer
 let playerScore = 0;
 let computerScore = 0;
 
+// Event listeners for button clicks
 document.getElementById("rockButton").addEventListener("click", function(){
-    playGame("rock")
+    playGame("rock");
 });
 
 document.getElementById("paperButton").addEventListener("click", function(){
-    playGame("paper")
+    playGame("paper");
 });
 
 document.getElementById("scissorsButton").addEventListener("click", function(){
-    playGame("scissors")
+    playGame("scissors");
 });
 
+// Function to play the game
 function playGame(playerChoice) {
+    // Get the computer's choice
     const computerChoice = getComputerChoice();
-    const result = playRound( playerChoice, computerChoice);
+    
+    // Play a round and display the result
+    const result = playRound(playerChoice, computerChoice);
     displayResult(result);
 }
 
-
+// Function to get a random choice for the computer
 function getComputerChoice(){
     let randomIndex = Math.floor(Math.random()* choices.length);
     let computerChoice = choices[randomIndex];
@@ -29,51 +36,65 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function playRound(playerChoice,computerChoice){
+// Function to play a single round of the game
+function playRound(playerChoice, computerChoice){
+    // Check the winner of the round
     const result = checkWinner(playerChoice, computerChoice);
-    if(result =="Tie"){
-        return "It's a Tie!"
-    }
-    else if(result == "player"){
-        return `You Win! ${playerChoice} beats ${computerChoice}`
-    }
-    else{
-        return `You lost! ${playerChoice} loses to ${computerChoice}`
-    }
-};
 
-function checkWinner(playerChoice,computerChoice ){
+    // Update scores based on the result
+    if(result == "player"){
+        playerScore += 1;
+    } else if(result == "Computer") {
+        computerScore += 1;
+    }
+
+    // Return a message based on the result
+    if(result == "Tie"){
+        return "It's a Tie!";
+    } else if(result == "player"){
+        return `You Win! ${playerChoice} beats ${computerChoice}`;
+    } else {
+        return `You lost! ${playerChoice} loses to ${computerChoice}`;
+    }
+}
+
+// Function to check the winner of a round
+function checkWinner(playerChoice, computerChoice){
+    // Check for a tie
     if (computerChoice == playerChoice) {
-        return "Tie"        
+        return "Tie";
     }
-    else if ((playerChoice == "rock" && computerChoice == "scissors") ||
-            (playerChoice == "paper" && computerChoice == "rock") ||
-            (playerChoice == "scissors" && computerChoice == "paper")){
-                playerScore += 1;
-                return "player"
+    // Check for player win conditions
+    else if (
+        (playerChoice == "rock" && computerChoice == "scissors") ||
+        (playerChoice == "paper" && computerChoice == "rock") ||
+        (playerChoice == "scissors" && computerChoice == "paper")
+    ) {
+        return "player";
     }
-    else  {
-        computerScore += 1;        
-        return "Computer"
+    // If not a tie and not a player win, it's a computer win
+    else {
+        return "Computer";
     }
-};
+}
 
+// Function to display the result and scores
 function displayResult(result) {
     const resultDisplay = document.getElementById("resultDisplay");
-    resultDisplay.textContent = `Player: ${playerScore} Computer: ${computerScore}\n
-    ${result}`;
-};
-
-
-
-
+    resultDisplay.textContent = `Player: ${playerScore} Computer: ${computerScore}\n${result}`;
+}
 
 /////////////////////////////////////////////////////////////////////////////////////
-//TEST CODE
+// TEST CODE (commented out for clarity)
 
+// Use this section for testing specific functionalities or scenarios
+
+// Example 1: Prompt user for input and display the result for one round
 // const playerChoice = prompt();
 // console.log(playerChoice);
+// playGame(playerChoice);
 
+// Example 2: Run the game for 5 rounds (old code)
 // for(i=1; i<6; i++) {
 //     const playerChoice = getPlayerChoice();
 //     const computerChoice = getComputerChoice();
@@ -81,25 +102,12 @@ function displayResult(result) {
 //     console.log(i)
 // }
 
-//OLD CODE
-
+// Old code for individual button clicks (commented out)
+// Use the new consolidated playGame function for improved readability and maintainability
 // document.getElementById("rockButton").addEventListener("click", function() {
 //     const playerChoice = "rock";
 //     const computerChoice = getComputerChoice();
 //     const result = playRound(playerChoice, computerChoice);
 //     displayResult(result);
 // });
-
-// document.getElementById("paperButton").addEventListener("click", function() {
-//     const playerChoice = "paper";
-//     const computerChoice = getComputerChoice();
-//     const result = playRound(playerChoice, computerChoice);
-//     displayResult(result); 
-// });
-
-// document.getElementById("scissorsButton").addEventListener("click", function() {
-//     const playerChoice = "scissors";
-//     const computerChoice = getComputerChoice();
-//     const result = playRound(playerChoice, computerChoice);
-//     displayResult(result);
-// });
+// ...
