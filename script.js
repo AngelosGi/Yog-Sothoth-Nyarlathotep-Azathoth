a903bd2ab9c713913cab6f9c0b617c202fa6cdc8
AngelosGi <144551151+AngelosGi@users.noreply.github.com>
2023-11-25 19:25:13 +0200

    add buttons

--- script.js
+++ script.js
@@ -2,12 +2,29 @@
 const choices = ["rock", "paper", "scissors"];
 
 
-function getPlayerChoice(){
-    let randomIndex = Math.floor(Math.random()* choices.length);
-    let playerChoice = choices[randomIndex];
-    console.log(playerChoice)
-    return playerChoice;
-}
+
+
+document.getElementById("rockButton").addEventListener("click", function() {
+    const playerChoice = "rock";
+    const computerChoice = getComputerChoice();
+    const result = playRound(playerChoice, computerChoice);
+    displayResult(result);
+});
+
+document.getElementById("paperButton").addEventListener("click", function() {
+    const playerChoice = "paper";
+    const computerChoice = getComputerChoice();
+    const result = playRound(playerChoice, computerChoice);
+    displayResult(result); 
+});
+
+document.getElementById("scissorsButton").addEventListener("click", function() {
+    const playerChoice = "scissors";
+    const computerChoice = getComputerChoice();
+    const result = playRound(playerChoice, computerChoice);
+    displayResult(result);
+});
+
 
 function getComputerChoice(){
     let randomIndex = Math.floor(Math.random()* choices.length);
@@ -45,13 +62,20 @@ function playRound(playerChoice,computerChoice){
     }
 };
 
+
+function displayResult(result) {
+    const resultDisplay = document.getElementById("resultDisplay");
+    resultDisplay.textContent = result;
+};
+
+
 // const playerChoice = prompt();
 // console.log(playerChoice);
 
-for(i=1; i<6; i++) {
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-    console.log(i)
-}
+// for(i=1; i<6; i++) {
+//     const playerChoice = getPlayerChoice();
+//     const computerChoice = getComputerChoice();
+//     console.log(playRound(playerChoice, computerChoice));
+//     console.log(i)
+// }
 
